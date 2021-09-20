002d00faa13ab8e0e87685b8cd74b54d4dce31af
LEVO <90807226+HydrLEVO@users.noreply.github.com>
2021-09-20 10:46:04 +1000

    Add files via upload

---
+++ Hard-Disk.py
@@ -0,0 +1,148 @@
+import platform
+import time
+import os
+import subprocess
+
+
+print("""
+                                           powerd by LEVO-Corporation
+                                        Levo.inc copyright non_stable 1.0
+""")
+select = input("")
+
+if select == 'run':
+    print("""
+                                                   ______________
+                                                  |              |
+                                                  |              |
+                                                  |    ######    |
+                                                  |   ########   |
+                                                  |   ########   |
+                                                  |    ######    |
+                                                  |              |
+                                                  |              |
+                                                  |  HARD-DISK   |
+                                                  | ***********  |
+                                                  |______________|
+                                                  
+                                               Booting from Hard-Disk...
+                                               
+                                         type '#' to enter process of Hard-Disk
+""")
+select = input("* ")
+
+if select == '#':
+    print("""
+                                                   ______________
+                                                  |              |
+                                                  |              |
+                                                  |    ######    |
+                                                  |   ###HD###   |
+                                                  |   ########   |
+                                                  |    ######    |
+                                                  |              |
+                                                  |     user     |
+                                                  |  HARD-DISK   |
+                                                  | ***********  |
+                                                  |______________|
+                                                  
+                                            importing H-D drive to Log
+                                                    
+                                               Booting from Hard-Disk...
+                                               
+                                          import process = already Satisfied
+                                          
+                                        
+                                           type 'HD' to enter Hard-Disk
+    """)
+select = input("* ")
+
+if select == 'hd':
+    print("""
+                                                   ______________
+                                                  |              |
+                                                  |              |
+                                                  |    ######    |
+                                                  |   ###HD###   |
+                                                  |   ########   |
+                                                  |    ######    |
+                                                  |              |
+                                                  |     user     |
+                                                  |  HARD-DISK   |
+                                                  | ***********  |
+                                                  |______________|
+                                          
+                                        
+                                                    DISK_ACTIVE
+    """)
+select = input("* ")
+
+if select == 'deactive':
+    print("""
+                                                   ______________
+                                                  |              |
+                                                  |              |
+                                                  |    ######    |
+                                                  |   ###**###   |
+                                                  |   ########   |
+                                                  |    ######    |
+                                                  |              |
+                                                  |     user     |
+                                                  |  HARD-DISK   |
+                                                  | ***********  |
+                                                  |______________|
+                                          
+                                        
+                                                   DISK_DEACTIVE
+    """)
+select = input("* ")
+
+if select == 'info':
+    print("this is a machine you can run by a HARD-DISK that you have setted up.")
+    
+if select == 'run':
+    print("""
+                                                  GNU_GRUB   HARD-DISK
+                                     _______________________________________________
+                                    |                                               |
+                                    | Hard-Disk_Boot                                |
+                                    | access to HARD-DISK                           |
+                                    | Hard-Disk_memory                              |
+                                    | Hard-Disk_storage                             |
+                                    |                                               |
+                                    |_______________________________________________|
+                                    
+                                                   POWERD BY LEVO
+    """)
+select = input("* ")
+if select == '3':
+    print("""
+                                           MEMORY = 13MS
+                                           MEMORY = 13GIGS
+""")
+
+if select == '1':
+    print("""
+                                             Boot-system Not active
+    """)
+select = input("* ")
+if select == '2':
+    print("""
+                                                   ______________
+                                                  |              |
+                                                  |              |
+                                                  |    ######    |
+                                                  |   ###**###   |
+                                                  |   ########   |
+                                                  |    ######    |
+                                                  |              |
+                                                  |     user     |
+                                                  |  HARD-DISK   |
+                                                  | ***********  |
+                                                  |______________|
+                                          
+                                        
+                                                   DISK_DEACTIVE
+    """)
+
+
